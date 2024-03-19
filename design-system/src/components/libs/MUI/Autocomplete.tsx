95c99edd4c38336e7c1cc459e7975fb0cd5fe7d2
jjjiyoung0130 <jiyoung63@nate.com>
2024-03-19 23:08:54 +0900

    feat: 추가사항 clone

--- design-system/src/components/libs/MUI/Autocomplete.tsx
+++ design-system/src/components/libs/MUI/Autocomplete.tsx
@@ -1,6 +1,7 @@
 import { Autocomplete, TextField } from "@mui/material";
 
 export const MUIAutoComplete = () => {
+  // 임의로 생성한 options
   const options = [
     "MUI",
     "TailwindCSS UI",
@@ -12,9 +13,9 @@ export const MUIAutoComplete = () => {
     <Autocomplete
       disablePortal // Autocomplete 메뉴가 포털에 렌더링되지 않고 Autocomplete 컴포넌트의 DOM 구조 내에 직접 렌더링
       id="combo-box"
-      options={options}
-      sx={{ width: 300 }}
-      renderInput={(params) => <TextField {...params} label="UI Library" />}
+      options={options} // options 배열을 전달하여 자동 완성할 옵션 설정
+      sx={{ width: 300 }} // 컴포넌트 스타일 설정
+      renderInput={(params) => <TextField {...params} label="UI Library" />} // 커스텀 입력 필드
     />
   );
 };
